f2641bf6b8e5320dd28be1cf12c685790519e6d9
walimorris <48896622+walimorris@users.noreply.github.com>
2020-02-11 15:16:33 -0800

    Add files via upload

--- PythonClub/club/models.py
+++ PythonClub/club/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Meeting(models.Model): 
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField()
+    meetinglocation=models.CharField(max_length=255)
+    meetingagenda=models.CharField(max_length=255)
+
+    def __str__(self): 
+        return self.meetingtitle
+
+    class Meta: 
+        db_table='meeting'
+        verbose_name_plural='meetings'
+
+class MeetingMinute(models.Model): 
+    meetingminuteId=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    meetingminuteattendance=models.ManyToManyField(User)
+    meetingminutetext=models.TextField()
+
+    def __str__(self): 
+        return self.meetingId
+
+    class Meta: 
+        db_table='meetingminute'
+        verbose_name_plural='meetingminutes'
+
+class Resource(models.Model): 
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255) 
+    resourceurl=models.URLField(null=True, blank=True)
+    resourceentrydate=models.DateField()
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription=models.CharField(max_length=255)
+
+
+    def __str__(self): 
+        return self.resourcename
+
+    class Meta: 
+        db_table='resource'
+        verbose_name_plural='resources'
+
+class Event(models.Model): 
+    eventtitle=models.CharField(max_length=255)
+    eventlocation=models.CharField(max_length=255)
+    eventdate=models.DateField()
+    eventtime=models.TimeField()
+    eventdescription=models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+
+    def __str__(self): 
+        return self.eventtitle
+
+    class Meta: 
+        db_table='event'
+        verbose_name_plural='events'
