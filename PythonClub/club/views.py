b553363f212ef152edffc1cec685cc603fbf0ffc
walimorris <48896622+walimorris@users.noreply.github.com>
2020-02-11 18:53:33 -0800

    Add files via upload

--- PythonClub/club/views.py
+++ PythonClub/club/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinute, Resource, Event
 
 # Create your views here.
 def index(request): 
     return render(request, 'club/index.html')
+
+def getresources(request): 
+    resource_list=Resource.objects.all()
+    return render(request, 'club/resources.html' ,{'resource_list' : resource_list})
+    
