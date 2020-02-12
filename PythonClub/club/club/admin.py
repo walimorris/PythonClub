1aceacd9f281da5b48ba65d5d2de54dfa99b89f4
walimorris <48896622+walimorris@users.noreply.github.com>
2020-02-11 18:51:14 -0800

    Delete admin.py

--- PythonClub/club/club/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Meeting, MeetingMinute, Resource, Event
-
-admin.site.register(Meeting)
-admin.site.register(MeetingMinute)
-admin.site.register(Resource)
-admin.site.register(Event)
